05c04959e0979ac781eba616fbf063ae28272aa4
Seba <sebastiansanchez.b20@gmail.com>
2025-03-17 02:44:42 -0300

    fix data footer

--- src/components/common/ContactForm.jsx
+++ src/components/common/ContactForm.jsx
@@ -19,7 +19,21 @@ export const ContactForm = () => {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log('form enviado')
+        const { nameClient, company, telNumber, email, description } = formData;
+        const destinatario = "support@sgonline.uy";
+        const subject = `Nuevo mensaje en SGonline.uy de contacto de ${nameClient}`;
+        const body = `
+        Nombre: ${nameClient}
+        Empresa: ${company || "Empresa no especificada"}
+        Teléfono: ${telNumber || "Teléfono no especificado"}
+        Email: ${email}
+        Descripción:
+        ${description}
+        `;
+    
+        const mailtoLink = `mailto:${destinatario}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    
+        window.location.href = mailtoLink;
     }
 
   return (
@@ -120,126 +134,3 @@ export const ContactForm = () => {
     </div>
   )
 }
-
-
-// import React,{useState} from 'react'
-// import { HiOutlineArrowRight } from "react-icons/hi2";
-
-// export const ContactForm = () => {
-
-//     const [formData,setFormData] = useState({
-//         nameClient:'',
-//         company:'',
-//         telNumber:'',
-//         email:'',
-//         description:''
-//     })
-
-//     const handleChange = (e) =>{
-//         const {name, value} = e.target;
-//         setFormData({...formData, [name]:value})
-//     }
-
-//     const handleSubmit = (e) =>{
-//         e.preventDefault();
-//         console.log('form enviado')
-//     }
-
-//   return (
-//     <div id="formContainer" className='flex items-center justify-center mt-20'>
-//         <form
-//             onSubmit={handleSubmit}
-//             className='w-full p-6 rounded-lg max-w-lg mx-auto '
-//         >
-
-//             <div className='mb-4 border-b-[2px] border-gray-700 hover:border-r-[2px]  hover:border-gray-700'>
-//                 {/* <label className='block text-gray-300 text-sm font-medium mb-6' htmlFor='name'>
-//                     Nombre y Apellido
-//                 </label> */}
-//                 <input
-//                     type='text'
-//                     id='name'
-//                     name='name'
-//                     value={FormData.nameClient}
-//                     onChange={handleChange}
-//                     placeholder='Nombre completo *'
-//                     className='form-input '
-//                     required
-//                     />
-//             </div>
-//             <div className='mb-4 border-b-[2px] border-gray-700 hover:border-r-[2px]  hover:border-gray-700'>
-//                 {/* <label className='block text-gray-300 text-sm font-medium mb-20' htmlFor='name'>
-//                     Marca o Empresa
-//                 </label> */}
-//                 <input
-//                     type='text'
-//                     id='company'
-//                     name='company'
-//                     value={FormData.company}
-//                     onChange={handleChange}
-//                     placeholder='Marca, empresa o idea!'
-//                     className='form-input '
-//                     />
-//             </div>
-//             <div className='mb-4 border-b-[2px] border-gray-700 hover:border-r-[2px]  hover:border-gray-700'>
-//                 {/* <label className='block text-gray-300 text-sm font-medium mb-20' htmlFor='name'>
-//                     Teléfono
-//                 </label> */}
-//                 <input
-//                     type='tel'
-//                     id='telNumber'
-//                     name='telNumber'
-//                     value={FormData.telNumber}
-//                     onChange={handleChange}
-//                     placeholder='Número de teléfono'
-//                     className='form-input '
-//                     />
-//             </div>
-//             <div className='mb-4 border-b-[2px] border-gray-700 hover:border-r-[2px]  hover:border-gray-700'>
-//                 {/* <label className='block text-gray-300 text-sm font-medium mb-20' htmlFor='name'>
-//                     Email
-//                 </label> */}
-//                 <input
-//                     type='email'
-//                     id='email'
-//                     name='email'
-//                     value={FormData.email}
-//                     onChange={handleChange}
-//                     placeholder='Email *'
-//                     className='form-input '
-//                     required
-//                     />
-//             </div>
-//             <div className="mb-4 border-b-[2px] border-gray-700 hover:border-[2px]hover:border-gray-700">
-//                 {/* <label className="block text-gray-300 text-sm font-medium mb-2" htmlFor="descripcion">
-//                     Descripción Breve
-//                 </label> */}
-//                 <textarea
-//                     id="description"
-//                     name="description"
-//                     value={formData.description}
-//                     onChange={handleChange}
-//                     placeholder="Describe brevemente lo que necesitas *"
-//                     className="form-input w-[100%]"
-//                     rows="4"
-//                     required
-//                 ></textarea>
-//             </div>
-
-//             <div>
-//           <button
-//                 type="submit"
-//                 className="`
-//                     w-[30%] bg-gray-300 hover:bg-gray-700 text-gray-600 font-bold py-2 px-4 rounded-lg 
-//                     border border-gray-700 transition duration-300
-//                     hover:text-gray-300
-//                     "
-//                 >
-//                     Enviar
-//                 </button>
-//             </div>
-//         </form>
-        
-//     </div>
-//   )
-// }
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,26 +4,33 @@ import { Link } from 'react-router-dom';
 
 export const Footer = () => {
   return (
-    <div className='w-full flex flex-col md:flex-row #000300 justify-between border-t-2 border-gray-500'>
-        <div className='flex flex-col m-8 md:m-20'>
-            <h2 className='font-bold text-white text-xl md:text-2xl font-montserrat'>Navegación</h2>
-            <Link to='/'><h3 className='h2-menu-footer'>Inicio</h3></Link>
-            <Link to='/portfolio'><h3 className='h2-menu-footer'>Works</h3></Link>
-            <Link to='/'><h3 className='h2-menu-footer'>Skills</h3></Link>
-            <Link to='/'><h3 className='h2-menu-footer'>Clients</h3></Link>
-        </div>
-        <div className='flex flex-col m-8 md:m-20'>
-            <h2 className='font-bold text-white text-xl md:text-2xl font-montserrat'>Nosotros</h2>
-            <h3 className='h2-menu-footer'>Montevideo, Uruguay</h3>
-            <h3 className='h2-menu-footer'>planner@sgonline.com.uy</h3>
-            <h3 className='h2-menu-footer'>+598 92 433 537</h3>
-            <div className='flex flex-row ml-4 gap-3'>
-                <a href='https://instagram.com/' target='_blank'><img src='/assets/images/logo-instagram.webp' className='size-12 md:grayscale hover:grayscale-0'/></a>
-                <a href='https://web-whatsapp.com/' target='_blank'><img src='/assets/images/logo-whatsapp.webp' className='size-12 md:grayscale hover:grayscale-0'/></a>
-        </div>
-        </div>
-        <div className='flex flex-row md:m-20 gap-3'>
-        </div>
+    <div className="w-full flex flex-col">
+
+      <div className='w-full flex flex-col md:flex-row justify-center md:gap-[12rem] border-t-2 border-gray-500 px-8 md:px-20'>
+          
+          <div className='flex flex-col mt-8 text-center md:text-left'>
+              <h2 className='font-bold text-white text-xl md:text-2xl font-montserrat'>Navegación</h2>
+              <Link to='/'><h3 className='h2-menu-footer'>Inicio</h3></Link>
+              <Link to='/portfolio'><h3 className='h2-menu-footer'>Works</h3></Link>
+              <Link to='/'><h3 className='h2-menu-footer'>Skills</h3></Link>
+              <Link to='/'><h3 className='h2-menu-footer'>Clients</h3></Link>
+          </div>
+
+          <div className='flex flex-col mt-8 text-center md:text-left'>
+              <h2 className='font-bold text-white text-xl md:text-2xl font-montserrat'>Nosotros</h2>
+              <h3 className='h2-menu-footer'>Montevideo, Uruguay</h3>
+              <h3 className='h2-menu-footer'>support@sgonline.uy</h3>
+              <h3 className='h2-menu-footer'>+598 92 433 537</h3>
+              <div className='flex flex-row justify-center md:justify-start gap-3 mt-2'>
+                  <a href='https://instagram.com/santigon.psd' target='_blank'><img src='/assets/images/logo-instagram.webp' className='size-12 md:grayscale hover:grayscale-0'/></a>
+                  <a href='http://wa.me/598924335377' target='_blank'><img src='/assets/images/logo-whatsapp.webp' className='size-12 md:grayscale hover:grayscale-0'/></a>
+              </div>
+          </div>
+      </div>
+
+      <div className='w-full flex justify-center items-center mt-4 py-4 border-t border-gray-700'>
+        <p className='font-montserrat text-sm text-gray-500 text-center'>Website desarrollado por SGonline</p>
+      </div>
     </div>
-  )
+  );
 }
